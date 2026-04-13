81a3ab2071c1a277691c1314693c70651544b28c
hilarionv <hilarion2004@gmail.com>
2026-04-13 21:19:50 +0000

    eeee

--- script.js
+++ script.js
@@ -1,40 +1,71 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    new Splide(".imprimantes", {
+  // SPLIDE
+  new Splide(".imprimantes", {
       perPage: 3,
       gap: "20px",
       breakpoints: {
-        768: { perPage: 1 }
+          768: { perPage: 1 }
       }
-    }).mount();
-  
-    new Splide(".ordinateurs", {
+  }).mount();
+
+  new Splide(".ordinateurs", {
       perPage: 3,
       gap: "20px",
       breakpoints: {
-        768: { perPage: 1 }
+          768: { perPage: 1 }
       }
-    }).mount();
-  
+  }).mount();
+
+
+  // AOS
+  AOS.init({
+      duration: 1500,
+      once: true,
+      offset: 80,
   });
 
 
-
-
-
-    AOS.init({
-        duration: 1500,
-      once: true,
-      offset: 80,
-
-    });
-
-    function toggleMenu() {
+  // MENU
+  window.toggleMenu = function () {
       const menu = document.getElementById("fullMenu");
-      if (menu.style.display === "flex") {
-          menu.style.display = "none";
-      } else {
-          menu.style.display = "flex";
-      }
+      menu.style.display = (menu.style.display === "flex") ? "none" : "flex";
+  };
+
+
+  // FAQ
+  const questions = document.querySelectorAll(".faq-question");
+
+  questions.forEach(question => {
+      question.addEventListener("click", () => {
+
+          const answer = question.nextElementSibling;
+
+          question.classList.toggle("active");
+
+          if (answer.style.maxHeight) {
+              answer.style.maxHeight = null;
+          } else {
+              answer.style.maxHeight = answer.scrollHeight + "px";
+          }
+      });
+  });
+
+
+  // PANIER
+  let count = 0;
+  const countSpan = document.getElementById('panier-count');
+  const addBtn = document.getElementById('add-to-cart');
+
+  if (addBtn && countSpan) {
+      addBtn.addEventListener('click', () => {
+          count++;
+          countSpan.textContent = count;
+
+          countSpan.classList.add('animate');
+          setTimeout(() => countSpan.classList.remove('animate'), 200);
+      });
   }
+
+});
   
