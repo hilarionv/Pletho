fd694e8a2165725b9330dd1450a933709ffd2177
hilarionv <hilarion2004@gmail.com>
2026-04-13 21:25:46 +0000

    eee

--- script.js
+++ script.js
@@ -68,4 +68,13 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
 });
+
+function toggleMenu() {
+  const menu = document.getElementById("fullMenu");
+  if (menu.style.display === "flex") {
+      menu.style.display = "none";
+  } else {
+      menu.style.display = "flex";
+  }
+}
   
